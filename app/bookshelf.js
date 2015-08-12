df2e7ea75c75ac63d1c2a126132ef89770ac3711
Maia Black <maia@Maias-MacBook-Pro.local>
2015-08-12 14:30:27 -0700

    Use bookshelf registry plugin to avoid circular dependencies

--- app/bookshelf.js
+++ app/bookshelf.js
@@ -3,4 +3,7 @@ var dbConfig = {
     connection: process.env.PG_CONNECTION_STRING
 };
 var pg = require('knex')(dbConfig);
-module.exports = require('bookshelf')(knex);
+var bookshelf = require('bookshelf')(knex);
+bookshelf.plugin('registry');
+
+module.exports = bookshelf;
